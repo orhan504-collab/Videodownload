9066ec5b4fcc7dadbba5d9f978c6943fb0c0eae5
orhan504-collab <orhan_504@hotmail.com>
2026-02-24 00:44:23 +0300

    Create video_model.dart

---
+++ lib/video_model.dart
@@ -0,0 +1,15 @@
+class VideoModel {
+  final String id;
+  final String title;
+  final String thumbnail;
+  final String duration;
+  final String url;
+
+  VideoModel({
+    required this.id, 
+    required this.title, 
+    required this.thumbnail, 
+    required this.duration,
+    required this.url,
+  });
+}
