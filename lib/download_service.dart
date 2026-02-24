410c03654284e3600d901d21b0bc5a72855fe98b
orhan504-collab <orhan_504@hotmail.com>
2026-02-24 19:47:58 +0300

    Update download_service.dart

--- lib/download_service.dart
+++ lib/download_service.dart
@@ -1,34 +1,26 @@
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'dart:io';
-import 'package:dio/dio.dart';
-import 'package:ffmpeg_kit_flutter_https/ffmpeg_kit.dart'; // Paket yolu güncellendi
 import 'package:path_provider/path_provider.dart';
 
 class DownloadService {
-  static Future<void> indirVeCevir(String url, String dosyaAdi, String format) async {
-    final directory = await getExternalStorageDirectory(); 
-    if (directory == null) return;
+  static final yt = YoutubeExplode();
 
-    final mp4Path = "${directory.path}/$dosyaAdi.mp4";
-    final outputPath = "${directory.path}/$dosyaAdi.$format";
-
-    Dio dio = Dio();
-
+  static Future<void> download(String id, String title, bool isMp3) async {
     try {
-      print("İndirme başladı...");
-      await dio.download(url, mp4Path);
-
-      // FFmpeg komutlarını tırnak içine alarak dosya isimlerindeki boşluk hatalarını engelliyoruz
-      if (format == "avi") {
-        await FFmpegKit.execute("-i \"$mp4Path\" -c copy \"$outputPath\"");
-      } else if (format == "mp3") {
-        await FFmpegKit.execute("-i \"$mp4Path\" -vn -ab 192k \"$outputPath\"");
-      }
+      var manifest = await yt.videos.streamsClient.getManifest(id);
+      var streamInfo = isMp3 ? manifest.audioOnly.withHighestBitrate() : manifest.muxed.bestQuality;
       
-      if (File(mp4Path).existsSync()) {
-        File(mp4Path).deleteSync(); 
-      }
+      var stream = yt.videos.streamsClient.get(streamInfo);
+      var dir = await getExternalStorageDirectory();
+      String cleanTitle = title.replaceAll(RegExp(r'[^\w\s]+'), '');
+      var file = File('${dir!.path}/$cleanTitle${isMp3 ? ".mp3" : ".mp4"}');
+      
+      var fileStream = file.openWrite();
+      await stream.pipe(fileStream);
+      await fileStream.close();
+      print("İndirildi: ${file.path}");
     } catch (e) {
-      print("İşlem hatası: $e");
+      print("Hata: $e");
     }
   }
 }
