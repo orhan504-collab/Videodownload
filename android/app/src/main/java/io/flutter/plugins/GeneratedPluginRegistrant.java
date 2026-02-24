be1d51ba7dbf0123af78fe3fb11770b39b1d1833
orhan504-collab <orhan_504@hotmail.com>
2026-02-24 22:25:51 +0300

    Update GeneratedPluginRegistrant.java

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,18 +1,1 @@
-package io.flutter.plugins;
 
-import androidx.annotation.Keep;
-import androidx.annotation.NonNull;
-import io.flutter.Log;
-import io.flutter.embedding.engine.FlutterEngine;
-
-@Keep
-public final class GeneratedPluginRegistrant {
-  private static final String TAG = "GeneratedPluginRegistrant";
-  public static void registerWith(@NonNull FlutterEngine flutterEngine) {
-    try {
-      flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
-    } catch (Exception e) {
-      Log.e(TAG, "Error registering plugin path_provider_android", e);
-    }
-  }
-}
