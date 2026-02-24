130e925ab4ae7eb2f71a05a648ddbcd76c9e62d5
orhan504-collab <orhan_504@hotmail.com>
2026-02-24 15:32:58 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/orhan504/videodownload/MainActivity.java
+++ android/app/src/main/java/com/orhan504/videodownload/MainActivity.java
@@ -3,4 +3,5 @@ package com.orhan504.videodownload;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    // v2 embedding için burası boş bırakılır, Flutter otomatik yönetir.
 }
